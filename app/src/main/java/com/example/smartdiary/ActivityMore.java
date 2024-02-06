ca3e88ba404a72f9bfaef8989ad17d57d0708603
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2024-02-07 02:25:55 +0530

    Create Interface

---
+++ app/src/main/java/com/example/smartdiary/ActivityMore.java
@@ -0,0 +1,14 @@
+package com.example.smartdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+    }
+}
