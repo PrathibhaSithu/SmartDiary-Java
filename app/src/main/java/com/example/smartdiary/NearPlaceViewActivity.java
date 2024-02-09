c532b50851da15c2585a300fe28ab1903416dec7
PrathiSithu <86074406+PrathibhaSithu@users.noreply.github.com>
2024-02-10 00:33:49 +0530

    Update Map Activity

--- app/src/main/java/com/example/smartdiary/NearPlaceViewActivity.java
+++ app/src/main/java/com/example/smartdiary/NearPlaceViewActivity.java
@@ -1,9 +1,22 @@
 package com.example.smartdiary;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,11 +24,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.smartdiary.databinding.ActivityNearPlaceViewBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class NearPlaceViewActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityNearPlaceViewBinding binding;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final int Request_Code = 101;
+    private double lat, lng;
+    ImageButton hospital, atm, rest, bank;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +43,97 @@ public class NearPlaceViewActivity extends FragmentActivity implements OnMapRead
         binding = ActivityNearPlaceViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        hospital = findViewById(R.id.hospital);
+        atm = findViewById(R.id.atm);
+        rest = findViewById(R.id.rest);
+        bank = findViewById(R.id.bank);
+
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getApplicationContext());
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        atm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                stringBuilder.append("location="+lat+","+lng);
+                stringBuilder.append("&radius=1000");
+                stringBuilder.append("&type=atm");
+                stringBuilder.append("&sensor=true");
+                stringBuilder.append("&key="+getResources().getString(R.string.google_api_key));
+
+                String url = stringBuilder.toString();
+                Object dataFetch[] = new Object[2];
+                dataFetch[0] = mMap;
+                dataFetch[1] = url;
+
+                FetchNearData fetchNearData = new FetchNearData();
+                fetchNearData.execute(dataFetch);
+            }
+        });
+
+        hospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                stringBuilder.append("location="+lat+","+lng);
+                stringBuilder.append("&radius=1000");
+                stringBuilder.append("&type=hospital");
+                stringBuilder.append("&sensor=true");
+                stringBuilder.append("&key="+getResources().getString(R.string.google_api_key));
+
+                String url = stringBuilder.toString();
+                Object dataFetch[] = new Object[2];
+                dataFetch[0] = mMap;
+                dataFetch[1] = url;
+
+                FetchNearData fetchNearData = new FetchNearData();
+                fetchNearData.execute(dataFetch);
+            }
+        });
+
+        rest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                stringBuilder.append("location="+lat+","+lng);
+                stringBuilder.append("&radius=1000");
+                stringBuilder.append("&type=restaurant");
+                stringBuilder.append("&sensor=true");
+                stringBuilder.append("&key="+getResources().getString(R.string.google_api_key));
+
+                String url = stringBuilder.toString();
+                Object dataFetch[] = new Object[2];
+                dataFetch[0] = mMap;
+                dataFetch[1] = url;
+
+                FetchNearData fetchNearData = new FetchNearData();
+                fetchNearData.execute(dataFetch);
+            }
+        });
+
+        bank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+                stringBuilder.append("location="+lat+","+lng);
+                stringBuilder.append("&radius=1000");
+                stringBuilder.append("&type=bank");
+                stringBuilder.append("&sensor=true");
+                stringBuilder.append("&key="+getResources().getString(R.string.google_api_key));
+
+                String url = stringBuilder.toString();
+                Object dataFetch[] = new Object[2];
+                dataFetch[0] = mMap;
+                dataFetch[1] = url;
+
+                FetchNearData fetchNearData = new FetchNearData();
+                fetchNearData.execute(dataFetch);
+            }
+        });
     }
 
     /**
@@ -44,8 +150,68 @@ public class NearPlaceViewActivity extends FragmentActivity implements OnMapRead
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        getCurrentLocation();
+    }
+
+    private void getCurrentLocation(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, Request_Code);
+            return;
+        }
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(60000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(5000);
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Toast.makeText(getApplicationContext(),"Location result is" + locationResult, Toast.LENGTH_SHORT).show();
+
+                if(locationResult == null){
+                    Toast.makeText(getApplicationContext(),"Current Location is Null" + locationResult, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                for(Location location : locationResult.getLocations()){
+                    if(location != null){
+                        Toast.makeText(getApplicationContext(),"Location is" + location.getLongitude(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            };
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+                if(location != null){
+
+                    lat = location.getLatitude();
+                    lng = location.getLongitude();
+
+                    LatLng latLng = new LatLng(lat, lng);
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                }
+            }
+        });
+        }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case Request_Code:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getCurrentLocation();
+                }
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
